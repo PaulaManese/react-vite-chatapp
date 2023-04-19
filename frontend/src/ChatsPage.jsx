4afd2cdf9a35d6821ac6e191001852baa8f92541
Paula Manese <paulamanese@gmail.com>
2023-04-19 22:01:35 +0200

    added files

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId='5892162c-7c72-4893-813e-a5f252cf824f'
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: '100%' }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
